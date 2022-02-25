7c7711017d84605129d387254b8b78365380e190
Migui14 <migui14914@gmail.com>
2022-02-25 20:01:07 +0100

    lab 3

--- arcade-demo/lab 3.py
+++ arcade-demo/lab 3.py
@@ -1,23 +1,50 @@
 import arcade
 
-def draw_nieve(x,y):
+def draw_muñeco_nieve(x,y):
+    arcade.draw_circle_filled(500 + x, 100 + y, 20, arcade.color.WHITE)
+    arcade.draw_circle_filled(500 + x, 125 + y, 15, arcade.color.WHITE)
+    arcade.draw_circle_filled(495 + x, 127 + y, 3, arcade.color.BLACK)
+    arcade.draw_circle_filled(505 + x, 127 + y, 3, arcade.color.BLACK)
+    arcade.draw_triangle_filled(500 + x, 123 + y, 500 + x, 118 + y, 520 + x, 120 + y, arcade.color.ORANGE)
+
+def draw_muñeco_nieve_2(x,y):
     arcade.draw_circle_filled(500 + x, 100 + y, 20, arcade.color.WHITE)
     arcade.draw_circle_filled(500 + x, 125 + y, 15, arcade.color.WHITE)
     arcade.draw_circle_filled(495 + x, 127 + y, 3, arcade.color.BLACK)
     arcade.draw_circle_filled(505 + x, 127 + y, 3, arcade.color.BLACK)
     arcade.draw_triangle_filled(500 + x, 123 + y, 500 + x, 118 + y, 480 + x, 120 + y, arcade.color.ORANGE)
 
+def draw_sol(x,y):
+    arcade.draw_circle_filled(425 + x, 325 + y, 75, arcade.color.YELLOW)
+
+def draw_nube(x,y):
+    arcade.draw_circle_filled(125 + x, 500 + y, 25, arcade.color.WHITE)
+    arcade.draw_circle_filled(160 + x, 525 + y, 25, arcade.color.WHITE)
+    arcade.draw_circle_filled(160 + x, 475 + y, 25, arcade.color.WHITE)
+    arcade.draw_circle_filled(200 + x, 525 + y, 25, arcade.color.WHITE)
+    arcade.draw_circle_filled(200 + x, 475 + y, 25, arcade.color.WHITE)
+    arcade.draw_circle_filled(235 + x, 500 + y, 25, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(150 + x, 210 + x, 525 + y, 475 + y, arcade.color.WHITE)
+
 
 def on_draw(delta_time):
     arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
 
     arcade.start_render()
+
+    draw_sol(0,on_draw.sol2_y + 650)
+    on_draw.sol2_y += -1
+
+    draw_sol(0,on_draw.sol1_y - 50)
+    on_draw.sol1_y += 1
+
+    draw_nube(on_draw.nube1_x + 250, 0)
+    draw_nube(on_draw.nube1_x - 250, 0)
+    draw_nube(on_draw.nube1_x - 750, 0)
+    on_draw.nube1_x += 1
+
     arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.BITTER_LIME)
 
-    arcade.draw_circle_filled(0, 0, 400, arcade.color.LIGHT_BLUE)
-    arcade.draw_lrtb_rectangle_filled(0, 800, 600, 200, arcade.color.AIR_SUPERIORITY_BLUE)
-
-    arcade.draw_circle_filled(425, 325, 75, arcade.color.YELLOW)
     arcade.draw_triangle_filled(300, 200, 1000, 200, 700, 500, arcade.color.GRAY)
     arcade.draw_triangle_filled(0, 200, 500, 200, 250, 450, arcade.color.DARK_GRAY)
 
@@ -36,17 +63,25 @@ def on_draw(delta_time):
     arcade.draw_triangle_filled(500, 237, 550, 237, 525, 267, arcade.color.GREEN)
     arcade.draw_triangle_filled(500, 225, 550, 225, 525, 255, arcade.color.GREEN)
 
-    draw_nieve(on_draw.nieve1_x, 90)
-    on_draw.nieve1_x += 1
+    arcade.draw_lrtb_rectangle_filled(720, 730, 225, 200, arcade.color.BROWN)
+    arcade.draw_triangle_filled(700, 250, 750, 250, 725, 280, arcade.color.GREEN)
+    arcade.draw_triangle_filled(700, 237, 750, 237, 725, 267, arcade.color.GREEN)
+    arcade.draw_triangle_filled(700, 225, 750, 225, 725, 255, arcade.color.GREEN)
 
-    arcade.draw_circle_filled(125, 500, 25, arcade.color.WHITE)
-    arcade.draw_circle_filled(160, 525, 25, arcade.color.WHITE)
-    arcade.draw_circle_filled(160, 475, 25, arcade.color.WHITE)
-    arcade.draw_circle_filled(200, 525, 25, arcade.color.WHITE)
-    arcade.draw_circle_filled(200, 475, 25, arcade.color.WHITE)
-    arcade.draw_circle_filled(235, 500, 25, arcade.color.WHITE)
-    arcade.draw_lrtb_rectangle_filled(150, 210, 525, 475, arcade.color.WHITE)
-on_draw.nieve1_x = 90
+
+    draw_muñeco_nieve_2(on_draw.muñeco_nieve_21_x, 50)
+    on_draw.muñeco_nieve_21_x -= 1
+
+    draw_muñeco_nieve(on_draw.muñeco_nieve2_x - 300, -50)
+    draw_muñeco_nieve(on_draw.muñeco_nieve2_x - 850, 0)
+    on_draw.muñeco_nieve2_x += 1
+
+on_draw.muñeco_nieve_21_x = 90
+on_draw.muñeco_nieve2_x = 90
+on_draw.sol1_y = 90
+on_draw.sol2_y = 90
+on_draw.nube1_x = 90
+
 
 def main():
     arcade.open_window(800, 600, "lab 3")
